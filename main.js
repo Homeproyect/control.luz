6674267fb5170f1250cc91727650d370b7554f3b
Homeproyect <113480886+Homeproyect@users.noreply.github.com>
2022-11-28 21:57:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function go (){
+     var usuario=document.getElementById("usuario").value; 
+   var contraseña= document.getElementById("contraseña").value;
+   
+  
+   
+  
+   
+      if(usuario=='Carlos' && contraseña== '1234') {
+       
+     location="on off/onoff.html";
+      
+      }
+      
+      else{
+          alert("el usuario o la contraseña es incorrecta por favor vuelve a intentarlo");
+          
+      }
+      }
+      
